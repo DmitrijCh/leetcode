05a72dfe2b23db036ab7de4e1477aa8468605bf2
dmitrij <dmitrij.chvanov@gmail.com>
2022-07-21 14:01:28 +0400

    task 26

---
+++ RemoveDuplicatesFromSortedArray/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] firstNumbers = {0,0,1,1,1,2,2,3,3,4};
+        int length = solution.removeDuplicates(firstNumbers);
+        System.out.println(length);
+
+        int[] secondNumbers = new int[length];
+        System.arraycopy(firstNumbers, 0, secondNumbers, 0, length);
+        System.out.println(Arrays.toString(secondNumbers));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                continue;
+            }
+            nums[count] = nums[i];
+            count++;
+        }
+        return count;
+    }
+}
+/*
+Input: [0,0,1,1,1,2,2,3,3,4]
+Output: 5, [0,1,2,3,4]
+ */
