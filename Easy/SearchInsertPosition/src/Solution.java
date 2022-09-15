c6a4cab32fff52852ea4353747b9e279bb6b438a
dmitrij <dmitrij.chvanov@gmail.com>
2022-09-15 14:19:52 +0400

    task 35

---
+++ Easy/SearchInsertPosition/src/Solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 3, 5, 6};
+        System.out.println(solution.searchInsert(nums, 5));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int result = 0;
+        if (nums.length == 1 && target > nums[0]) {
+            return 1;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (target == nums[i]) {
+                result = i;
+                break;
+            } else if (target > nums[i] && target <= nums[i + 1]) {
+                result = i + 1;
+                break;
+            } else if (target > nums[i + 1] && (nums.length - 1 == i + 1)) {
+                result = nums.length;
+                break;
+            } else if (target < nums[i] && i == 0) {
+                break;
+            }
+        }
+        return result;
+    }
+}
+
+/*
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+ */
