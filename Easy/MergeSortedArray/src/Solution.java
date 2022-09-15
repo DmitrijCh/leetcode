0458a5425f5a6b8d36b6a3b5bd0c5b2a2f9f423a
dmitrij <dmitrij.chvanov@gmail.com>
2022-09-15 14:49:47 +0400

    task 88

---
+++ Easy/MergeSortedArray/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        solution.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = 0;
+        for (int i = m; i < nums1.length; i++) {
+            nums1[i] = nums2[j];
+            j++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
+/*
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+ */
